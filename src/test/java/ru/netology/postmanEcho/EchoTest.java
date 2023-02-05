729bf21298cd7812f2bb5c1d86fda0188cac6aa9
Ilya Zakharov QAMID-50 <oiska@bk.ru>
2023-02-05 19:42:04 +0300

    Fail test

---
+++ src/test/java/ru/netology/postmanEcho/EchoTest.java
@@ -0,0 +1,18 @@
+package ru.netology.postmanEcho;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class EchoTest {
+    @Test
+    void shouldCheckSomeData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some"));
+    }
+}
